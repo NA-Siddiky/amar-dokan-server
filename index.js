acb537adde0bd7c6817a91f907323d17e0040224
Naimul Alam <naimul2707@gmail.com>
2021-05-29 01:50:34 +0200

    10 products updated to db and primary two api done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const cors = require("cors");
+const ObjectID = require("mongodb").ObjectID;
+
 const MongoClient = require("mongodb").MongoClient;
+
 require("dotenv").config();
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dspyj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterph1.yvjd9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const port = process.env.PORT || 5000;
 
@@ -14,8 +16,39 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 app.get("/", (req, res) => {
   res.send("Welcome! To the Amar dokan Server...");
 });
 
-app.listen(port, () => {console.log("Hello")});
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productCollection = client.db("AmarDokan").collection("products");
+  // perform actions on the collection object
+
+  // all products
+  app.get("/products", (req, res) => {
+    productCollection.find({}).toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  // single product by id
+  app.get("/products/:id", (req, res) => {
+    productCollection
+      .find({ _id: ObjectID(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
