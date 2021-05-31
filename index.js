638ff5c86ff2c61f981aa3d0f18217a7345a8225
NA-Siddiky <siddiky.academic@gmail.com>
2021-05-31 15:33:19 +0600

    user adding

--- index.js
+++ index.js
@@ -25,6 +25,48 @@ client.connect((err) => {
   const productCollection = client.db("AmarDokan").collection("products");
   // perform actions on the collection object
 
+  app.post('/addUser', async (req, res) => {
+    const user = req.body;
+    const { email } = user;
+    usersCollection.findOne({ email }, (err, data) => {
+
+      if (data) {
+        const { isSignedIn, name, email, } = data;
+        console.log(email)
+        // adminsCollection.findOne({ adminEmail: email }, (err, adminData) => {
+        //   if (adminData) {
+
+        //     const makeAdmin = async (email) => {
+        //       await usersCollection.updateOne({ "email": email }, { $set: { "role": 'admin' } })
+        //     }
+        //     makeAdmin(email);
+        //     const payload = {
+        //       isSignedIn,
+        //       name,
+        //       email,
+        //       role: "admin"
+        //     }
+        //     res.send(payload);
+        //   } else {
+        //     res.send(data);
+        //   }
+        // })
+
+      } else {
+        usersCollection.insertOne(user)
+          .then(result => {
+            if (result.insertedCount > 0) {
+              res.send(result.ops[0])
+            }
+          })
+      }
+    })
+  })
+
+
+
+
+
   // all products
   app.get("/products", (req, res) => {
     productCollection.find({}).toArray((err, items) => {
